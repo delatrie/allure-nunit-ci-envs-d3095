3dc85f6166f90969b6bb8f454c312357fe4960f3
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2025-08-27 18:37:15 +0700

    Step 4 - Convert env variable to Allure label

--- Allure.Examples.CiEnvs.Tests/BaseTestClass.cs
+++ Allure.Examples.CiEnvs.Tests/BaseTestClass.cs
@@ -1,3 +1,4 @@
+using Allure.Net.Commons;
 using Allure.NUnit;
 
 namespace Allure.Examples.CiEnvs.Tests;
@@ -5,5 +6,13 @@ namespace Allure.Examples.CiEnvs.Tests;
 [AllureNUnit]
 class BaseTestClass
 {
-
+    [TearDown]
+    public void AddEnvLabelIfDefined()
+    {
+        var value = Environment.GetEnvironmentVariable($"ALLURE_LABEL_env");
+        if (!string.IsNullOrEmpty(value))
+        {
+            AllureApi.AddLabel("env", value);
+        }
+    }
 }
