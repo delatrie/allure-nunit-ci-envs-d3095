75e59ca1c52cb28b2b613ecbc3e6ad9e776c6653
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2025-09-01 16:57:37 +0700

    Step 5 - Define environments in Allure config

--- allurerc.mjs
+++ allurerc.mjs
@@ -1,9 +1,62 @@
+const hasEnv = (labels, expectedEnv) =>
+  labels.some(({ name, value }) => name === "env" && value === expectedEnv);
+
 export default {
   plugins: {
     awesome: {
       options: {
         groupBy: ["package", "testClass"],
       },
+    },
+  },
+  environments: {
+    "ubuntu-22.04, net8.0, Debug": {
+      matcher: ({ labels }) => hasEnv(labels, "ubuntu-22.04, net8.0, Debug"),
+    },
+    "ubuntu-22.04, net8.0, Release": {
+      matcher: ({ labels }) => hasEnv(labels, "ubuntu-22.04, net8.0, Release"),
+    },
+    "ubuntu-22.04, net9.0, Debug": {
+      matcher: ({ labels }) => hasEnv(labels, "ubuntu-22.04, net9.0, Debug"),
+    },
+    "ubuntu-22.04, net9.0, Release": {
+      matcher: ({ labels }) => hasEnv(labels, "ubuntu-22.04, net9.0, Release"),
+    },
+    "ubuntu-24.04, net8.0, Debug": {
+      matcher: ({ labels }) => hasEnv(labels, "ubuntu-24.04, net8.0, Debug"),
+    },
+    "ubuntu-24.04, net8.0, Release": {
+      matcher: ({ labels }) => hasEnv(labels, "ubuntu-24.04, net8.0, Release"),
+    },
+    "ubuntu-24.04, net9.0, Debug": {
+      matcher: ({ labels }) => hasEnv(labels, "ubuntu-24.04, net9.0, Debug"),
+    },
+    "ubuntu-24.04, net9.0, Release": {
+      matcher: ({ labels }) => hasEnv(labels, "ubuntu-24.04, net9.0, Release"),
+    },
+    "windows-2022, net8.0, Debug": {
+      matcher: ({ labels }) => hasEnv(labels, "windows-2022, net8.0, Debug"),
+    },
+    "windows-2022, net8.0, Release": {
+      matcher: ({ labels }) => hasEnv(labels, "windows-2022, net8.0, Release"),
+    },
+    "windows-2022, net9.0, Debug": {
+      matcher: ({ labels }) => hasEnv(labels, "windows-2022, net9.0, Debug"),
+    },
+    "windows-2022, net9.0, Release": {
+      matcher: ({ labels }) => hasEnv(labels, "windows-2022, net9.0, Release"),
+    },
+    "windows-2025, net8.0, Debug": {
+      matcher: ({ labels }) => hasEnv(labels, "windows-2025, net8.0, Debug"),
+    },
+    "windows-2025, net8.0, Release": {
+      matcher: ({ labels }) => hasEnv(labels, "windows-2025, net8.0, Release"),
+    },
+    "windows-2025, net9.0, Debug": {
+      matcher: ({ labels }) => hasEnv(labels, "windows-2025, net9.0, Debug"),
+    },
+    "windows-2025, net9.0, Release": {
+      matcher: ({ labels }) => hasEnv(labels, "windows-2025, net9.0, Release"),
     },
   },
 };
