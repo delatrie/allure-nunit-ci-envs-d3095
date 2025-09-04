5542745f2c7c0d6fe8d97e38df50476eef4920c0
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2025-09-05 03:30:06 +0700

    Step 4 - Split and group results by envs

--- Allure.Examples.CiEnvs.Tests/BaseTestClass.cs
+++ Allure.Examples.CiEnvs.Tests/BaseTestClass.cs
@@ -1,3 +1,4 @@
+using Allure.Net.Commons;
 using Allure.NUnit;
 
 namespace Allure.Examples.CiEnvs.Tests;
@@ -5,5 +6,19 @@ namespace Allure.Examples.CiEnvs.Tests;
 [AllureNUnit]
 class BaseTestClass
 {
+    [TearDown]
+    public void AddEnvParameterIfDefined()
+    {
+        var value = Environment.GetEnvironmentVariable($"ALLURE_LABEL_env");
+        if (!string.IsNullOrEmpty(value))
+        {
+            // Group by environments on the 'Suites' tab
+            AllureApi.AddParentSuite(value);
 
+            // Parameterizing tests with environment info keeps the results from different environments separated.
+            // See https://allurereport.org/docs/history-and-retries/#common-pitfall-tests-from-different-environments-are-missing-in-the-report.
+            // (the parameter can be hidden by adding `ParameterMode.Hidden` as the third argument)
+            AllureApi.AddTestParameter("env", value);
+        }
+    }
 }
